812106df17f5c103b49258ae983e87727d498adf
Kasra Torshizi <ktorsh@terpmail.umd.edu>
2024-04-22 16:57:03 -0400

    HUH

--- RNLF/tools/psnr.m
+++
@@ -1,3 +0,0 @@
-function res = psnr(ima1, ima2)
-
-res = 10 * log10(255^2 / mean((ima1(:) - ima2(:)).^2));
--- RNLF/tools/read_video.m
+++
@@ -1,5 +0,0 @@
-function file = read_video(path_name, img_name)
-
-obj = VideoReader([path_name img_name]);
-file = double(read(obj,[1 Inf]));
-file = squeeze(file(:,:,1,:));
